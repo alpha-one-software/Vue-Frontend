aa2186a11a13e1a338cba36c6ced43086f36e211
Alpha One Software <49074891+alpha-one-software@users.noreply.github.com>
2024-05-01 02:13:21 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ app.use(CKEditor);
 app.use(ElementPlus);
 app.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyDCDe_kzdqziechOJ53yViPLc6hLQbhX1o",
+    key: "",
     libraries: "places",
   },
 });
